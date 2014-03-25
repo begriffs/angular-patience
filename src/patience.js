81cbb29998087b692fc4878bb41d40acdbbf8bfa
Joe Nelson <cred+github@begriffs.com>
2014-03-24 21:32:04 -0700

    Intercept all requests for now

--- src/patience.js
+++ src/patience.js
@@ -3,20 +3,22 @@
 
   angular.module('begriffs.patience', []).
 
-    factory('patience', ['$http', '$q', function ($http, $q) {
-      return {
-        response: function(response) {
-          console.log(response);
-          return response || $q.when(response);
-        },
+    config(['$httpProvider', function ($httpProvider) {
+      $httpProvider.interceptors.push(['$q',
+        function ($q) {
+          return {
+            response: function(response) {
+              return response || $q.when(response);
+            },
 
-        responseError: function(rejection) {
-          // if (canRecover(rejection)) {
-          //   return responseOrNewPromise;
-          // }
-          return $q.reject(rejection);
-        }
-      };
+            responseError: function(rejection) {
+              // if (canRecover(rejection)) {
+              //   return responseOrNewPromise;
+              // }
+              return $q.reject(rejection);
+            }
+          };
+        }]);
     }]);
 
 }());
--- test/patience.js
+++ test/patience.js
@@ -1,19 +1,38 @@
 (function () {
   'use strict';
 
-  var $httpBackend, $httpProvider;
+  var $http, $httpBackend;
   beforeEach(function () {
     angular.mock.module('begriffs.patience');
 
     angular.mock.inject(
-      ['$httpBackend', '$httpProvider',
-      function (httpBackend, httpProvider) {
+      ['$http', '$httpBackend',
+      function (http, httpBackend) {
+        $http = http;
         $httpBackend = httpBackend;
-        $httpProvider = httpProvider;
-
-        $httpProvider.interceptors.push('patience');
       }]
     );
+  });
+
+  describe('the patience interceptor', function () {
+    it('passes normal responses through', function () {
+      var handlers = {
+        success: function (data, status) {
+          expect(status).toEqual(200);
+        },
+        error: function () { }
+      };
+      spyOn(handlers, 'success').andCallThrough();
+      spyOn(handlers, 'error');
+
+      $http.get('/slow').success(handlers.success).error(handlers.error);
+
+      $httpBackend.expectGET('/slow').respond(200, '');
+      $httpBackend.flush();
+
+      expect(handlers.success).toHaveBeenCalled();
+      expect(handlers.error).not.toHaveBeenCalled();
+    });
   });
 
 }());
